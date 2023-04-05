e123d661aaf2b9edc872bde56748caf5c97f8d81
Bhavesh Patel <tonyb760@gmail.com>
2023-04-05 14:53:48 +0300

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+
+/**
+  * _sqrt_recursion - Should return the natural square root of a number
+  * @n: this is the number to calculate the natural square root
+  *
+  * Return: value of natural square root
+  */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+  * _sqrt - Used to calculate natural square root
+  * @n: value to calculate the square root
+  * @i: iterate number
+  *
+  * Return: natural square root
+  */
+int _sqrt(int n, int i)
+{
+	int sqrt = i * i;
+
+	if (sqrt > n)
+		return (-1);
+
+	if (sqrt == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
