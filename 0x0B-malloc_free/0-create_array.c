b8e209bdb84a6b50da6acbcf5cbaff812e8e9488
Bhavesh Patel <tonyb760@gmail.com>
2023-04-11 23:39:05 +0300

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,15 +14,15 @@ char *arr;
 unsigned int i;
 
 if (size == 0)
-return NULL;
+return (NULL);
 
 arr = malloc(size * sizeof(char));
 
 if (arr == NULL)
-return NULL;
+return (NULL);
 
 for (i = 0; i < size; i++)
 arr[i] = c;
 
-return arr;
+return (arr);
 }
