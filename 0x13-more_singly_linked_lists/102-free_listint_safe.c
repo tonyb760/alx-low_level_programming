94d69c9a3c32398a690287fa98dfd8a19b59c52c
Bhavesh Patel <tonyb760@gmail.com>
2023-05-02 22:50:45 +0300

    102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -50,45 +50,114 @@ hare = (hare->next)->next;
 
 return (0);
 }
-
 /**
-* free_listint_safe - Frees a listint_t list safely (i.e.,
+* free_listint_safe - Frees a listint_t list safely (i.e.
 *                     can free lists containing loops).
 * @h: A pointer to the address of the head of the listint_t list.
 *
 * Return: The size of the list that was freed.
-*
-* Description: The function sets the head to NULL after freeing the list.
 */
 size_t free_listint_safe(listint_t **h)
 {
-listint_t *tmp;
-size_t nodes, index;
+listint_t *tmp, *tortoise, *hare;
+size_t nodes = 0, loop_size = 0;
 
-nodes = count_nodes_in_looped_list(*h);
+if (h == NULL || *h == NULL)
+return (0);
 
-if (nodes == 0)
+tortoise = *h;
+hare = (*h)->next;
+
+while (hare != NULL && hare->next != NULL)
 {
-for (; h != NULL && *h != NULL; nodes++)
+if (tortoise == hare)
 {
-tmp = (*h)->next;
-free(*h);
-*h = tmp;
+loop_size = count_loop_size(tortoise);
+break;
 }
+
+tortoise = tortoise->next;
+hare = hare->next->next;
+}
+
+if (loop_size > 0)
+{
+nodes = count_nodes(*h, tortoise, loop_size);
 }
 else
 {
-for (index = 0; index < nodes; index++)
+while (*h != NULL)
 {
 tmp = (*h)->next;
 free(*h);
 *h = tmp;
+nodes++;
+}
 }
 
 *h = NULL;
+return (nodes);
 }
 
-h = NULL;
+/**
+* count_loop_size - Counts the number of nodes in a loop of a linked list.
+* @node: A pointer to a node in the loop.
+*
+* Return: The number of nodes in the loop.
+*/
+size_t count_loop_size(listint_t *node)
+{
+listint_t *tmp = node;
+size_t size = 1;
 
-return (nodes);
+while (tmp->next != node)
+{
+size++;
+tmp = tmp->next;
+}
+
+return (size);
+}
+
+/**
+* count_nodes - Counts the number of nodes in a linked list.
+* @head: A pointer to the head of the linked list.
+* @node: A pointer to a node in the loop of the linked list, or NULL.
+* @loop_size: The number of nodes in the loop of the linked list, or 0.
+*
+* Return: The number of nodes in the linked list.
+*/
+size_t count_nodes(listint_t *head, listint_t *node, size_t loop_size)
+{
+listint_t *tmp = head, *last = NULL;
+size_t count = 0;
+
+while (tmp != node)
+{
+last = tmp;
+tmp = tmp->next;
+free(last);
+count++;
+}
+
+if (loop_size > 0)
+{
+while (loop_size-- > 0)
+{
+last = tmp;
+tmp = tmp->next;
+free(last);
+count++;
+}
+}
+
+while (tmp != NULL)
+{
+last = tmp;
+tmp = tmp->next;
+free(last);
+count++;
+}
+
+return (count);
 }
